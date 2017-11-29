61f1206eb599167dbd218039b2e96de307839c1a
snava10 <sergio.nava89@gmail.com>
2017-11-29 13:48:48 +0000

    creating a static library

---
+++ cpp/libraries/src/calc.cpp
@@ -0,0 +1,23 @@
+#include "calc.h"
+
+/*
+double Calc::sqrt(double x){
+	return 0;
+}
+
+double Calc::sin(double x){
+	return sin(x);
+}
+
+double Calc::cos(double x){
+	return cos(x);
+}
+
+double Calc::pow(double base, double exp){
+	return 0;
+}*/
+
+double Calc::add(double a, double b){
+	return a+b;
+}
+
