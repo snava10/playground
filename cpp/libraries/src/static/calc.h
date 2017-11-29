fe9bf1df4a050ce202e52e8f001af6a987d59a7f
snava10 <sergio.nava89@gmail.com>
2017-11-29 16:22:22 +0000

    creating a static and a dynamic library

---
+++ cpp/libraries/src/static/calc.h
@@ -0,0 +1,13 @@
+#ifndef CALC_H
+#define CALC_H
+
+namespace Calc
+{
+	double add(double, double);
+//	double sqrt(double);
+//	double sin(double);
+//	double cos(double);
+//	double pow(double, double);
+}
+
+#endif
---
+++ cpp/libraries/src/static/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "calc.h"
+
+using namespace std;
+
+int main(){
+	double a = 100;
+	cout << Calc::add(10,2);
+	cout << "\n";
+}
