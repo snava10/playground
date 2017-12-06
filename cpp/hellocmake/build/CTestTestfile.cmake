1265c1fe82b751f2ba365e20ce6c091373a1ae78
snava10 <sergio.nava89@gmail.com>
2017-12-06 17:30:42 +0000

    downloading the code to use cmake with libraries

--- cpp/hellocmake/build/CTestTestfile.cmake
+++ cpp/hellocmake/build/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/sergio/github/playgroud/cpp/hellocmake
-# Build directory: /home/sergio/github/playgroud/cpp/hellocmake/build
+# Source directory: /home/god_admin/github/playgroud/cpp/hellocmake
+# Build directory: /home/god_admin/github/playgroud/cpp/hellocmake/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
--- cpp/hellocmake/build/cmake_install.cmake
+++ cpp/hellocmake/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/sergio/github/playgroud/cpp/hellocmake
+# Install script for directory: /home/god_admin/github/playgroud/cpp/hellocmake
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/sergio/github/playgroud/cpp/hellocmake/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/god_admin/github/playgroud/cpp/hellocmake/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
