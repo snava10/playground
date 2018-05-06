2b2a45bdb97c9ca48a37c4976b3d7da625a27eb3
snava10 <sergio.nava89@gmail.com>
2018-05-06 14:34:03 +0100

    old changes and some docker stuff

--- cpp/hellocmake/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/god_admin/github/playgroud/cpp/hellocmake
-# Build directory: /home/god_admin/github/playgroud/cpp/hellocmake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(toDoTest "toDo")
