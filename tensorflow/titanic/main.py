00e97784b9f69d96853cd549ea8b6a5b26a52865
snava10 <sergio.nava89@gmail.com>
2018-08-26 12:52:04 +0100

    main

---
+++ tensorflow/titanic/main.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def read_data():
+    train = pd.read_csv('data/train.csv')
+    train['Sex'] = train['Sex'].apply(lambda sex: 0 if sex=='male' else 1)
+    train['Fare'] = train['Fare'] / train['Fare'].max()
+    print(train[['Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']])
+    # print(train)
+
+
+if __name__ == '__main__':
+    read_data()
