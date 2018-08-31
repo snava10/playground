4097268c1b82cfd181f2e532e1113d0ecc178bd9
snava10 <sergio.nava89@gmail.com>
2018-08-31 22:32:59 +0100

    moving things around

--- tensorflow/source/titanic/main.py
+++ tensorflow/source/titanic/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def read_data():
     train = pd.read_csv('data/train.csv')
-    train['Sex'] = train['Sex'].apply(lambda sex: 0 if sex=='male' else 1)
+    train['Sex'] = train['Sex'].apply(lambda sex: 0 if sex == 'male' else 1)
     train['Fare'] = train['Fare'] / train['Fare'].max()
     print(train[['Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']])
     # print(train)
