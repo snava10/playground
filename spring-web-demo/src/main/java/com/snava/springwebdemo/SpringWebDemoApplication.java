7691e2883cabc4b7399fbe0cd3ba60f53d1310d8
Sergio Navarrete <sergio.nava89@gmail.com>
2021-09-28 10:40:31 +0100

    some opencv stuff and kubernetes deployment for spring boot

---
+++ spring-web-demo/src/main/java/com/snava/springwebdemo/SpringWebDemoApplication.java
@@ -0,0 +1,23 @@
+package com.snava.springwebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		System.out.println("Hitting hello endpoint");
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ spring-web-demo/src/test/java/com/snava/springwebdemo/SpringWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snava.springwebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
